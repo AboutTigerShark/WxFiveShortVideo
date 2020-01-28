c81cf32ee1c08bdba0dcb9354c87d003ef35c028
TigerShark <781977456@qq.com>
2020-01-28 16:54:35 +0800

    first

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //app.js
 App({
-
+  serverUrl: "http://192.168.0.110:8081",
+  userinfo: null
 })
--- pages/userLogin/userLogin.js
+++ pages/userLogin/userLogin.js
@@ -1,66 +1,68 @@
 // pages/userLogin/userLogin.js
+const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
 
   },
+  doLogin: function(e){
+    var formobject = e.detail.value;
+    var username = formobject.username;
+    var password = formobject.password;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    var serverUrl = app.serverUrl;
 
+    //判空
+    if (username.length == 0 || password.length == 0) {
+      wx.showToast({
+        title: '用户名或密码不能为空',
+        icon: 'none',
+        duration: 3000
+      })
+    }else{
+      wx.showLoading({
+        title: '登陆中...'
+      });
+      wx.request({
+        url: serverUrl + '/login',
+        method: 'POST',
+        data: {
+          username: username,
+          password: password
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function (res) {
+          console.log(res.data);
+          wx.hideLoading();
+          var status = res.data.status;
+          if (status == 200) {
+            wx.showToast({
+              title: '登录成功',
+              icon: 'none',
+              duration: 3000
+            })
+            app.userInfo = res.data.data;
+          } else if (status == 500) {
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'none',
+              duration: 3000
+            })
+          }
+
+        }
+      })
+    }
+  
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goRegistPage: function(e){
+    wx.navigateTo({
+      url: '../userRegister/userRegister',
+    })
   }
+
 })
